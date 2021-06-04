780ea5081b0c7d39d4612efa964db7710a560fcf
tomaroyal <royal17310@iiitd.ac.in>
2021-06-05 00:41:57 +0530

    add files

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../global.postcss';
+	import ToggleTheme from '$lib/ToggleTheme.svelte';
+
+</script>
+
+<main>
+	<div
+		class="flex items-center justify-between w-full max-w-4xl mx-auto my-0 text-gray-900 sticky-nav md:my-8 bg-opacity-60 dark:text-gray-100"
+	>
+		<ToggleTheme />
+	</div>
+	<div class="p-8">
+		<slot />
+	</div>
+</main>
+
+<style lang="postcss">
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ProjectCard from '$lib/ProjectCard.svelte';
+
+</script>
+
+<h1 class="text-5xl font-extrabold mb-2">Hey, I'm Royal Tomar</h1>
+<p>I'm a UX designer, creator. You've found my personal space on the Internet</p>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 md:p-8">
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+</div>
