f4641e619e3bb975ce8cc5c850ac7c94529e26dd
tomaroyal <royal17310@iiitd.ac.in>
2021-06-05 19:46:09 +0530

    shift navbar to component

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ToggleTheme from '$lib/ToggleTheme.svelte';
+
+</script>
+
+<div
+	class="flex items-center justify-between w-full max-w-4xl mx-auto my-0 text-gray-900 sticky-nav md:my-8 bg-opacity-60 dark:text-gray-100"
+>
+	<ToggleTheme />
+	<div class="space-x-4">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</div>
+</div>
