cc594a510daad1ec68ab1b3f4e4c0a697f7ec4ec
tomaroyal <royal17310@iiitd.ac.in>
2021-06-05 19:46:27 +0530

    create about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<div>
+	<h1 class="text-5xl font-extrabold mb-4">My Story</h1>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum, lectus sed suscipit
+		gravida, ante turpis vestibulum metus, sed blandit ipsum felis ac ipsum. Morbi ligula turpis,
+		bibendum quis commodo ut, suscipit nec dolor. Phasellus pharetra dolor nulla, id ultrices leo
+		lacinia sit amet. Vestibulum et massa ornare, dapibus ante quis, fermentum neque. Curabitur
+	</p>
+	<p>
+		rutrum. Morbi tortor nisl, bibendum vitae aliquet sit amet, euismod eget velit. Morbi sit amet
+		nibh posuere, condimentum nisl in, fringilla ex. In eu viverra nunc. Donec suscipit ac nulla
+		mattis fermentum. Vivamus placerat felis sit amet ex vulputate, a varius mauris malesuada. In ac
+		luctus leo, mollis ornare tellus. Suspendisse potenti. Vestibulum a consequat justo. Curabitur
+	</p>
+</div>
