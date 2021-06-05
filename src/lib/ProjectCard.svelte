0fca1c85df3cef8869c87fe0a108009146f6949b
tomaroyal <royal17310@iiitd.ac.in>
2021-06-05 19:48:36 +0530

    experiment with image only cards

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -1,13 +1,15 @@
 <div
 	class="transform hover:scale-105 transition duration-500 ease-in-out bg-white rounded-lg shadow-lg"
 >
-	<img
-		src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
-		alt=""
-		class="rounded-t-lg"
-	/>
-	<div class="p-4">
+	<a href="https://www.google.com" target="_blank">
+		<img
+			src="https://images.unsplash.com/photo-1600054800747-be294a6a0d26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80"
+			alt=""
+			class="rounded-lg"
+		/>
+	</a>
+	<!-- <div class="p-4">
 		<h2 class="font-bold mb-2 text-xl text-purple-800">Card with an image</h2>
 		<p class="text-purple-700 mb-2">This is a little bit better of a card!</p>
-	</div>
+	</div> -->
 </div>
