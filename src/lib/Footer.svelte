a34786109697dfff1340c7827d99e4724d5bf266
tomaroyal <royal17310@iiitd.ac.in>
2021-06-05 19:45:51 +0530

    add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,16 @@
+<div class="flex flex-col md:flex-row justify-between w-full py-4">
+	<div class="flex flex-col">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</div>
+	<div class="flex flex-col">
+		<a href="https://www.behance.net/tomaroyal" target="_blank">Behance</a>
+		<a href="https://twitter.com/_royaltomar" target="_blank">Twitter</a>
+		<a href="https://twitter.com/_royaltomar" target="_blank">Instagram</a>
+	</div>
+	<div class="flex flex-col">
+		<a href="https://www.linkedin.com/in/tomaroyal" target="_blank">LinkedIn</a>
+		<a href="https://github.com/tomaroyal" target="_blank">Github</a>
+		<a href="howdyroyall@gmail.com" target="_blank">Gmail</a>
+	</div>
+</div>
